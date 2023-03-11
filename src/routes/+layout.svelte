1af9672518a98f7efa7d9f5e91d7be6e09335351
JustMrMendez <mrmendez@amvestor.com>
2023-03-11 18:54:38 -0500

    new theme, pictures

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
-	// import '../theme.postcss';
+	// import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	import '../theme.postcss';
 	import '@skeletonlabs/skeleton/styles/all.css';
 	import '../app.postcss';
 
